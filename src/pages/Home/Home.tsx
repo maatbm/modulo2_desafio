bd23cda157e96550a358416af0d773809404b6d0
maatbm <matheusbmoraes0@gmail.com>
2025-03-01 18:35:26 -0300

    V1.0 - Criação inicial do projeto

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { MainContainer } from "./style";
+
+export function Home() {
+  return (
+    <>
+      <MainContainer>
+        <div className="LogoContainer"></div>
+        <div className="InputContainer"></div>
+      </MainContainer>
+    </>
+  );
+}
