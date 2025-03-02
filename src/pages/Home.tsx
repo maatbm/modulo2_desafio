3e1c56d88a524a98f704e26825fe5728c6b6e536
maatbm <matheusbmoraes0@gmail.com>
2025-03-01 23:50:46 -0300

    V1.1 - estrutura de estilização otimizada

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Main, Div } from "../styles/Global";
+
+export function Home() {
+  return (
+    <>
+      <Main>
+        <Div width="65%" backgroundColor="--blue"></Div>
+        <Div width="35%"></Div>
+      </Main>
+    </>
+  );
+}
