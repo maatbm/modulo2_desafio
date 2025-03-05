a1c4986d7b70c3e8d578ea1790b2fdb787d33157
maatbm <matheusbmoraes0@gmail.com>
2025-03-05 14:31:29 -0300

    Responsividade da página única de cada repositório finalizada

--- src/styles/pages/ReposStyle.ts
+++ src/styles/pages/ReposStyle.ts
@@ -6,9 +6,14 @@ export const Main = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
+  max-height: 100%;
   background-color: var(--light-blue);
   padding: 2rem 3rem;
+
+  @media screen and (max-width: 481px) {
+    padding: 1rem 0.7rem;
+  }
 `;
 
 export const Container = styled.div`
@@ -71,6 +76,60 @@ export const Container = styled.div`
           font-size: 1rem;
           font-weight: 400;
           color: var(--black);
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 481px) {
+    padding: 1rem;
+
+    h1 {
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+    }
+
+    > div {
+      padding: 0.5rem;
+
+      div {
+        width: 100%;
+        padding: 1rem;
+
+        h2 {
+          font-size: 1.2rem;
+        }
+
+        div {
+          width: 100%;
+          padding: 0.3rem;
+
+          h4 {
+            font-size: 1rem;
+          }
+
+          p {
+            font-size: 0.6rem;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 481px) and (max-width: 1120px) {
+    padding: 1rem;
+
+    > div {
+      div {
+        width: 100%;
+        padding: 1rem;
+
+        h2 {
+          font-size: 1.2rem;
+        }
+
+        div {
+          width: 100%;
         }
       }
     }
