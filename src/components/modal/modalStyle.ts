ccafd4151c68287ddca2a30c66827d186a643ebf
maatbm <matheusbmoraes0@gmail.com>
2025-03-06 08:04:24 -0300

    Fechar modal

--- src/components/modal/modalStyle.ts
+++ src/components/modal/modalStyle.ts
@@ -1,21 +1,42 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    max-height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(5px);
-    padding: 3rem;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 9999;
+interface CloseModalProps {
+  isOpen: boolean;
+}
 
-    @media screen and (max-width: 600px) {
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  max-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+  padding: 3rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 99;
+
+  @media screen and (max-width: 600px) {
     padding: 1rem 0.7rem;
+  }
+`;
+
+export const CloseModal = styled.div<CloseModalProps>`
+  width: 70px;
+  height: 70px;
+  margin: 1rem;
+  display: ${(props) => (props.isOpen ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-zindex: 100;
+
+  img {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
   }
 `;
--- src/components/repoCard/RepoCard.tsx
+++ src/components/repoCard/RepoCard.tsx
@@ -1,7 +1,9 @@
 import { Container } from "./RepoCardStyle";
 import { useState } from "react";
 import { Modal } from "../modal/Modal";
+import { CloseModal } from "../modal/modalStyle";
 import { Repos } from "../repos/Repos";
+import closeLogo from "../../assets/repoCard/close.png"
 
 interface Props {
   name: string;
@@ -15,12 +17,7 @@ export function RepoCard(UserData: Props) {
 
   return (
     <>
-      <Container
-        role="button"
-        onClick={() => {
-          setModal(true);
-        }}
-      >
+      <Container role="button" onClick={() => {setModal(true);}}>
         <h3>Repositório {UserData.index}</h3>
         <hr />
         <div>
@@ -31,6 +28,9 @@ export function RepoCard(UserData: Props) {
         </div>
       </Container>
       <Modal isOpen={modal}>
+        <CloseModal isOpen={modal} onClick={()=>{setModal(false)}}>
+          <img src={closeLogo} alt="close" />
+        </CloseModal>
         <Repos repoID={UserData.id} />
       </Modal>
     </>
