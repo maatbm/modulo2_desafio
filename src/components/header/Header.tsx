1047e8d0b55ba202de44d17f7aaeede3f0f3af87
maatbm <matheusbmoraes0@gmail.com>
2025-03-07 19:45:54 -0300

    Header atualizado

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,9 @@
 import { Container } from "./HeaderStyle";
 import logo from "../../assets/header/logoHeader.png";
-import volta from "../../assets/header/de-volta.png";
-import { useNavigate } from "react-router-dom";
 
 export function Header() {
-  const navigate = useNavigate();
   return (
     <Container>
-      <img role="button" src={volta} onClick={()=>navigate(-1)} id="goBack"/>
       <img src={logo} alt="Logo da wtech" />
     </Container>
   );
--- src/components/header/HeaderStyle.ts
+++ src/components/header/HeaderStyle.ts
@@ -4,27 +4,14 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   background-color: ${(props) => props.theme.colors.white};
-  gap: 0.5rem;
   padding: 1rem;
 
   img {
     width: 190px;
     height: auto;
-    margin-left: 2rem;
 
     @media screen and (max-width: 481px){
       margin-left: 0.5rem;
-    }
-  }
-
-  #goBack {
-    width: 40px;
-    height: 40px;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
-
-    &:hover {
-      transform: scale(1.2);
     }
   }
 
