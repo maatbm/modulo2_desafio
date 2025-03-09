edbec479ee255c4ffe3d43ba9a685c29b08e8a1b
maatbm <matheusbmoraes0@gmail.com>
2025-03-09 19:34:45 -0300

    Otimizações

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,14 @@
 import { API } from "./API";
 
 interface UserNameProps {
-    UserName: string;
+  UserName: string;
 }
 
-export async function GetUser({UserName}: UserNameProps) {
-    try {
-        const response = await API.get(`/${UserName}`);
-        return response.data;
-    } catch (error) {
-        throw new Error(error as string);
-    }
+export async function GetUser({ UserName }: UserNameProps) {
+  try {
+    const response = await API.get(`/${UserName}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error as string);
+  }
 }
