c41f69e8e0a35b0214268d7699fdd653f298590f
maatbm <matheusbmoraes0@gmail.com>
2025-03-09 08:38:35 -0300

    Otimizações

--- src/components/modal/modalStyle.ts
+++ src/components/modal/modalStyle.ts
@@ -1,9 +1,5 @@
 import styled from "styled-components";
 
-interface CloseModalProps {
-  isOpen: boolean;
-}
-
 export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
@@ -17,34 +13,4 @@ export const Container = styled.div`
   top: 0;
   left: 0;
   z-index: 99;
-
-  @media screen and (max-width: 600px) {
-    padding: 0.5rem;
-  }
-`;
-
-export const CloseModal = styled.div<CloseModalProps>`
-  width: 70px;
-  height: 70px;
-  margin: 1rem;
-  display: ${(props) => (props.isOpen ? "block" : "none")};
-  position: fixed;
-  top: 0;
-  right: 0;
-  z-zindex: 100;
-
-  img {
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
-
-    &:hover {
-      transform: scale(1.1);
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    padding: 0.2rem;
-  }
 `;
